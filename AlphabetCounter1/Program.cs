70c4dc5e2e32558c0318ae8267913cad18070cff
Edvard <Edvard@KOMPKRASAVCIKA>
2023-04-14 15:51:31 +0400

    Add project files.

---
+++ AlphabetCounter1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AlphabetCounter1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a word: ");
+            string word = Console.ReadLine();
+            word = word.ToLower();
+
+            Console.WriteLine("Write a lettter: ");
+            char letter = Convert.ToChar(Console.ReadLine());
+            letter = char.ToLower(letter);
+
+            int counter = 0;
+
+            foreach (var ch in word)
+            {
+                if (ch == letter)
+                    counter++;
+            }
+
+            Console.WriteLine($"You have {counter} of letter {letter}");
+        }
+    }
+}
